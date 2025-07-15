77ae7b566e897bbb22893d4300cbd7dd580b42bc
Sm1321 <siddulamohan1321@gmail.com>
2025-07-15 22:28:38 +0530

    will update tmrw-

--- src/states/blogstate.py
+++ src/states/blogstate.py
@@ -2,12 +2,9 @@ from typing import TypedDict
 from pydantic import BaseModel,Field
 
 
-
-
-
 class Blog(BaseModel):
     title:str = Field(description = "The Title of the BLog Post")
-    content:str = Field(description = "The main contnet of the blog post")
+    content:str = Field(description = "The main content of the blog post")
 
 
 class BlogState(TypedDict):
